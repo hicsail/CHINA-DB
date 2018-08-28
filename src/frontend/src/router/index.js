1f2d21b515c309ffc701e5ea10a22db801ec9af9
Ben <bengetch@gmail.com>
2018-08-28 14:20:57 -0400

    notfound page

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/Home'
 import about from '@/components/About'
+import notfound from '@/components/NotFound'
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       name: 'About',
       component: about
     },
+		{
+			path: '*',
+			name: 'NotFound',
+			component: notfound
+		}
   ],
   mode: 'history',
   linkActiveClass: 'is-active'
