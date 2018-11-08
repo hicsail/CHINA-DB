e0dd7d2ea77e3b3d6ab380e2335b9e2d89eb0c05
Ben <bengetch@gmail.com>
2018-11-08 14:36:44 -0500

    display filtered data

--- src/components/Geo.vue
+++ src/components/Geo.vue
@@ -3,23 +3,43 @@
         <div class="row">
             <h3>Shanxi Province, China</h3>
             <div class="col-md-9">
-                <div id="map" class="map">
-                    <l-map style="height: 100%" :zoom="zoom" :center="center">
-                        <l-tile-layer :url="url" :attribution="attribution"/>
+                <div id="map">
+                    <l-map
+                            style="height: 100%"
+                            :zoom="zoom"
+                            :center="center">
+                        <l-tile-layer
+                                :url="url"
+                                :attribution="attribution"/>
                         <l-marker
                                 v-for="marker in markers"
                                 :key="marker.id"
                                 :lat-lng="marker.position"
                                 :visible="marker.visible"
                                 :icon="icon"
+                                @click="pushPoints(marker)"
                         />
                     </l-map>
                 </div>
             </div>
             <div class="col-md-3">
-                <div id="filter-window" class="filter-window">
-                    <vue-slider v-model="sliderVals.value" v-bind="sliderVals"/>
-                    <button v-on:click="filterData">Filter</button>
+                <div id="filter-window">
+                    <vue-slider
+                            v-model="sliderVals.value"
+                            v-bind="sliderVals"
+                    />
+                    <button v-on:click="filterData">
+                        Filter
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="results-box">
+                    <li v-for="pt in renderedData">
+                        {{ pt }}
+                    </li>
                 </div>
             </div>
         </div>
@@ -34,8 +54,6 @@
 	import 'bootstrap/dist/css/bootstrap.css'
 	import 'bootstrap-vue/dist/bootstrap-vue.css'
     import "leaflet/dist/leaflet.css"
-
-
 
 	function findPoint(coords) {
 		for (let i = 0; i < data.coords.features.length; i++) {
@@ -60,8 +78,8 @@
 				sliderVals:
 					{
 						min: 1600,
-						max: 1910,
-						value: [1600, 1910],
+						max: 1930,
+						value: [1600, 1930],
 						formatter: "{value}",
 						mergeFormatter: "{value1} ~ {value2}",
 						tooltip: "always",
@@ -78,7 +96,8 @@
 				url: 'https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWJvdWNoYXVkIiwiYSI6ImNpdTA5bWw1azAyZDIyeXBqOWkxOGJ1dnkifQ.qha33VjEDTqcHQbibgHw3w',
 				attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 				markers: [],
-				pointData: []
+				pointData: {},
+                renderedData: []
 			}
 		},
 		methods: {
@@ -87,39 +106,55 @@
 			},
 			pushPoints(pt)
 			{
-				this.pointData.push(pt);
+				this.renderedData = [];
+				this.renderedData = this.pointData[pt.id];
 			},
             filterData()
             {
+            	/*
+            	Loop over point data, display points that contain
+            	 */
             	this.markers = [];
+            	let newMarkerFlag = true;
 
             	let yearLowerBound = this.sliderVals.value[0];
             	let yearUpperBound = this.sliderVals.value[1];
             	let featureArray = data.coords.features;
 
-            	let ids = 0;
-
                 for (let i = 0; i < featureArray.length; i++)
                 {
-                	let year = featureArray[i].properties.objects[0].time.start_year;
-                	if (year > yearLowerBound && year < yearUpperBound)
+                	let dataArray = featureArray[i].properties.objects;
+                	for (let j = 0; j < dataArray.length; j++)
                     {
-                    	let newLon = featureArray[i].geometry.coordinates[0];
-                    	let newLat = featureArray[i].geometry.coordinates[1];
+                    	let pointId = featureArray[i].id;
+                    	let year = featureArray[i].properties.objects[j].time.start_year;
+                        if (year > yearLowerBound && year < yearUpperBound)
+                        {
+                        	if (newMarkerFlag)
+                        	{
 
-                    	let newMarker =
-                          {
-                          	id: ids.toString(),
-                    		position: [newLat, newLon],
-                            visible: true
-                          };
-                    	this.markers.push(newMarker);
-                    	ids++;
+                              // create new entry in pointData dict for this point
+                              this.pointData[pointId] = [];
+
+                              let newLon = featureArray[i].geometry.coordinates[0];
+                              let newLat = featureArray[i].geometry.coordinates[1];
+
+                              let newMarker =
+                                {
+                                  id: pointId,
+                                  position: [newLat, newLon],
+                                  visible: true
+                                };
+                              this.markers.push(newMarker);
+                              newMarkerFlag = false;
+                            }
+
+                            // push this data to entry in pointData
+                            this.pointData[pointId].push(featureArray[i].properties.objects[j]);
+                        }
                     }
-                }
-                for ( let i = 0; i < this.markers.length; i++)
-                {
-                	console.log(this.markers[i]);
+                    newMarkerFlag = true;
+
                 }
             },
 		}
@@ -136,6 +171,10 @@
     }
 
     #filter-window {
+        border: 3px solid green;
+    }
+
+    #results-box {
         border: 3px solid green;
     }
 
