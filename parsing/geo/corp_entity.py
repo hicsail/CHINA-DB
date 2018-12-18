e0083f2ca45d4192963e39fe9abd0028782e6da1
Ben <bengetch@gmail.com>
2018-12-18 17:07:43 -0500

    corporate entity parsing

--- parsing/geo/corp_entity.py
+++ parsing/geo/corp_entity.py
@@ -15,12 +15,71 @@ class CorpEntityParser(Parser):
         self.corporate_entity_table = self.load_record("corporate_entity")
         self.corporate_entity_type_table = self.load_record("corporate_entity_type")
 
+    def nationality_dict(self):
+
+        ret = {}
+
+        for rec in self.nationality_table.keys():
+
+            this_record = self.nationality_table[rec]
+
+            try:
+                corp_entities = this_record["corporate_entity"]
+            except KeyError:
+                # missing person field, skip to next entry
+                continue
+
+            for c in corp_entities:
+                ret[c] = this_record["country_en"]
+
+        return ret
+
     def map_to_coords(self):
 
         ret = []
 
-        return ''
+        corp_to_nationality = self.nationality_dict()
 
-    def build_records(self):
+        for c in self.corporate_entity_table:
 
-        records = self.map_to_coords()
+            # initialize dict that stores info about this institution & populate below
+            c_rec = \
+                {
+                    "coords":
+                        {
+                            "lat": "N/A",
+                            "lon": "N/A"
+                        },
+                    "time":
+                        {
+                            "start_year": "N/A"
+                        },
+                    "loc":
+                        {
+                            "location_type": "N/A",
+                            "location_name": "N/A"
+                        },
+                    "type": "corporate entity",
+                    "corp_entity_type": "N/A",
+                    "nationality": "N/A",
+                    "tradition": "N/A",
+                    "denomination": "N/A",
+                    "name": "N/A",
+                }
+
+            try:
+                c_rec["time"]["start_year"] = self.corporate_entity_table[c]["china_start_year"]
+            except KeyError:
+                continue
+
+            try:
+                c_rec["nationality"] = corp_to_nationality[c].lower()
+            except KeyError:
+                pass
+
+
+
+
+
+
+
