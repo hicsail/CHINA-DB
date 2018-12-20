9b9b7ec929309baea0708eef046837d291f55ec3
Ben <bengetch@gmail.com>
2018-12-20 15:55:28 -0500

    geo coords for child institutions

--- parsing/geo/corp_entity.py
+++ parsing/geo/corp_entity.py
@@ -3,6 +3,8 @@ CorpEntity class -- Iterates over 'corporate_entity' table and produces a separa
 JSON object for each with geographical coordinates, along with other relevant information.
 """
 
+import copy
+
 from parsing.geo import Parser
 
 
@@ -13,6 +15,7 @@ class CorpEntityParser(Parser):
         super(CorpEntityParser, self).__init__(input_dir)
 
         self.corporate_entity_table = self.load_record("corporate_entity")
+        self.org_org_rel_type_table = self.load_record("organization_organization_rel_type")
 
     def nationality_dict(self):
 
@@ -50,6 +53,43 @@ class CorpEntityParser(Parser):
 
         return ret
 
+    def fetch_rel_type(self, rel_type_id):
+
+        try:
+            rel_type_rec = self.org_org_rel_type_table[rel_type_id]
+            return rel_type_rec["org_org_rel_type_en"]
+        except KeyError:
+            return ''
+
+    def add_geo(self, org_org_ids, rec):
+
+        ret = []
+
+        for ooid in org_org_ids:
+
+            try:
+                inst_id = self.org_org_table[ooid]["inst_id_1"][0]
+                inst_name = self.institution_table[inst_id]["name"]
+                rec["child_inst_name"] = inst_name
+                geo= self.institution_table[inst_id]["geography"]
+
+                for g in geo:
+
+                    rec_copy = copy.deepcopy(rec)
+                    geo_rec = self.fetch_geo(g)
+
+                    if geo_rec is not None:
+
+                        rec_copy["coords"]["lat"] = geo_rec["coords"]["lat"]
+                        rec_copy["coords"]["lon"] = geo_rec["coords"]["lon"]
+                        rec_copy["loc"]["location_type"] = geo_rec["loc"]["location_type"]
+                        rec_copy["loc"]["location_name"] = geo_rec["loc"]["location_name"]
+                        ret.append(rec_copy)
+            except KeyError:
+                continue
+
+        return ret
+
     def map_to_coords(self):
 
         ret = []
@@ -82,6 +122,7 @@ class CorpEntityParser(Parser):
                     "tradition": "N/A",
                     "denomination": "N/A",
                     "name": "N/A",
+                    "child_inst_name": "N/A"
                 }
 
             try:
@@ -100,6 +141,14 @@ class CorpEntityParser(Parser):
                     c_rec["corp_entity_type"].append(corp_to_type[t])
             except KeyError:
                 pass
+
+            try:
+                org_org_ids = self.corporate_entity_table[c]["organization_organization 2"]
+                recs = self.add_geo(org_org_ids, c_rec)
+                if len(recs) > 0:
+                    ret.extend(recs)
+            except KeyError:
+                continue
 
         return ret
 
