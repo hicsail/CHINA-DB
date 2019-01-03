56196720c56db1c2154405d29d67951552f5f457
Ben <bengetch@gmail.com>
2019-01-03 15:55:07 -0500

    - parse corp to geo coords - add tradition parsing

--- parsing/geo/person.py
+++ parsing/geo/person.py
@@ -40,15 +40,39 @@ class PersonParser(Parser):
 
         return ret
 
-    def add_geo(self, inst_id, rec):
+    def tradition_mapping(self, inst_rec):
+
+        ret = \
+            {
+                "religious_family": "N/A",
+                "corp_name": "N/A"
+            }
+
+        try:
+            org_org_id = inst_rec["organization_organization"][0]
+            rel_record = self.org_org_table[org_org_id]
+            corp_rel = rel_record["corp_id_2"]
+            this_corp = self.corporate_entity_table[corp_rel]
+
+            ret["corp_name"] = this_corp["corp_name_en"]
+            ret["religious_family"] = self._religious_family_mapping(this_corp["religious_family"][0])
+        except KeyError:
+            pass
+
+        return ret
+
+    def add_geo_inst(self, inst_id, rec):
         """
-        Fetch geographical coordinates for this record.
+        Fetch geographical coordinates for this record via the Institution that
+        it corresponds to.
         """
 
         ret = []
 
         current_institution = self.institution_table[inst_id]
         rec["institution_name"] = current_institution["inst_id"].lower()
+        rec["tradition"] = self.tradition_mapping(current_institution)
+
         geo = current_institution["geography"]
 
         for g in geo:
@@ -63,6 +87,52 @@ class PersonParser(Parser):
 
             if geo_rec is not None:
                 ret.append(rec_copy)
+
+        return ret
+
+    def add_geo_corp(self, corp_id, rec):
+        """
+        Fetch geographical coordinates for this record via the Corporate
+        Entity that it corresponds to.
+        """
+
+        ret = []
+
+        current_corp = self.corporate_entity_table[corp_id]
+
+        try:
+            org_org_ids = current_corp["organization_organization 2"]
+            corp_name = current_corp["corp_name_en"]
+            religious_family = self._religious_family_mapping(current_corp["religious_family"][0])
+        except KeyError:
+            return []
+
+        for ooid in org_org_ids:
+
+            try:
+                inst_id = self.org_org_table[ooid]["inst_id_1"][0]
+                inst_name = self.institution_table[inst_id]["name"]
+                geo = self.institution_table[inst_id]["geography"]
+
+                rec["institution_name"] = inst_name
+                rec["tradition"]["corp_name"] = corp_name
+                rec["tradition"]["religious_family"] = religious_family
+
+                for g in geo:
+
+                    rec_copy = copy.deepcopy(rec)
+                    geo_rec = self.fetch_geo(g)
+
+                    if geo_rec is not None:
+
+                        rec_copy["coords"]["lat"] = geo_rec["coords"]["lat"]
+                        rec_copy["coords"]["lon"] = geo_rec["coords"]["lon"]
+                        rec_copy["loc"]["location_type"] = geo_rec["loc"]["location_type"]
+                        rec_copy["loc"]["location_name"] = geo_rec["loc"]["location_name"]
+                        ret.append(rec_copy)
+
+            except KeyError:
+                continue
 
         return ret
 
@@ -104,6 +174,11 @@ class PersonParser(Parser):
                             "family_name_py": "N/A",
                             "given_name_py": "N/A"
                         },
+                    "tradition":
+                        {
+                            "religious_family": "N/A",
+                            "corp_name": "N/A"
+                        },
                     "type": "person",
                     "nationality": "N/A",
                     "gender": "N/A",
@@ -144,7 +219,9 @@ class PersonParser(Parser):
 
             for org in orgs:
 
+                # org can correspond to an institution or a corporate entity
                 current_org = self.person_org_table[org]
+
                 try:
                     p_ret["time"]["start_year"] = self.person_org_table[org]["start_year"]
                 except KeyError:
@@ -152,7 +229,15 @@ class PersonParser(Parser):
 
                 try:
                     inst_id = current_org["inst_id"][0]
-                    recs = self.add_geo(inst_id, p_ret)
+                    recs = self.add_geo_inst(inst_id, p_ret)
+                    if len(recs) > 0:
+                        ret.extend(recs)
+                except KeyError:
+                    pass
+
+                try:
+                    corp_id = current_org["corp_id"][0]
+                    recs = self.add_geo_corp(corp_id, p_ret)
                     if len(recs) > 0:
                         ret.extend(recs)
                 except KeyError:
