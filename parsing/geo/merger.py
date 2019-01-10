bf304f2b1fd67af1c93ccd2de7c1d84586ac0614
Ben <bengetch@gmail.com>
2019-01-10 12:20:00 -0500

    finish up merge parser

--- parsing/geo/merger.py
+++ parsing/geo/merger.py
@@ -1,4 +1,6 @@
 import os
+import json
+import pystache
 
 from parsing.geo.corp_entity import CorpEntityParser
 from parsing.geo.institution import InstitutionParser
@@ -19,11 +21,26 @@ class MergeParser:
         self.inst_parser = InstitutionParser()
         self.person_parser = PersonParser()
 
-    def merge_all(self):
+        self.records = None
+
+    def create_all(self):
+        """
+        Generate all records
+        """
 
         self.corp_parser.build_records()
         self.inst_parser.build_records()
         self.person_parser.build_records()
+
+        return self
+
+    def merge_all(self):
+        """
+        Merge all records into a single list of point records
+        """
+
+        if self.records is None:
+            self.create_all()
 
         all_records = self.corp_parser.records + self.inst_parser.records + self.person_parser.records
 
@@ -38,14 +55,81 @@ class MergeParser:
             if coords in all_coords:
                 # merge this record with an existing Point
 
-                '''
                 del r["coords"]
 
-                ret[coords]["properties"]["objects"].append(r)
-                '''
+                if r["type"] == "person":
+                    ret[coords]["properties"]["persons"].append(r)
+                elif r["type"] == "corporate_entity":
+                    ret[coords]["properties"]["corporate_entities"].append(r)
+                elif r["type"] == "institution":
+                    ret[coords]["properties"]["institutions"].append(r)
+                elif r["type"] == "event":
+                    ret[coords]["properties"]["events"].append(r)
+                else:
+                    print("Encountered unknown type: {}".format(r["type"]))
+                    continue
+
             else:
 
                 all_coords.add(coords)
 
                 new_coords = [r["coords"]["lon"], r["coords"]["lat"]]
                 del r["coords"]
+
+                new_dict = \
+                    {
+                        "type": "Feature",
+                        "id": str(id_num),
+                        "geometry":
+                            {
+                                "type": "Point",
+                                "coordinates": new_coords
+                            },
+                        "properties":
+                            {
+                                "persons": [],
+                                "institutions": [],
+                                "corporate_entities": [],
+                                "events": []
+                            }
+                    }
+
+                if r["type"] == "person":
+                    new_dict["properties"]["persons"].append(r)
+                elif r["type"] == "corporate_entity":
+                    new_dict["properties"]["corporate_entities"].append(r)
+                elif r["type"] == "institution":
+                    new_dict["properties"]["institutions"].append(r)
+                elif r["type"] == "event":
+                    new_dict["properties"]["events"].append(r)
+                else:
+                    print("Encountered unknown type: {}".format(r["type"]))
+                    continue
+
+                ret[coords] = new_dict
+                id_num += 1
+
+        self.records = list(ret.values())
+
+        return self
+
+    def write_records(self, out_path=None):
+        """
+        Write GeoJson formatted records to file
+        """
+
+        if self.records is None:
+            self.merge_all()
+
+        if out_path is None:
+            out_path = "/tmp/geo_all.js"
+
+        template = open("{}/geo.tmpl".format(self.template_dir)).read()
+
+        data = \
+            {
+                "DATA": json.dumps(self.records, indent=4, sort_keys=False)
+            }
+
+        with open(out_path, 'w', encoding='utf8') as f:
+            f.write(pystache.render(template, data))
