67925f36d2c462cdc500790bb6de97c1af61ef0d
SarahL88 <sarahlei@bu.edu>
2019-01-10 08:40:29 -0500

    Capitalized first letter in popup.

--- src/components/mixins/popupContent.js
+++ src/components/mixins/popupContent.js
@@ -15,17 +15,35 @@ export const PopupContent = {
        * @return       an HTML string to use as popup content
        */
 
-      let start = '<div> <table> <tbody>';
-      let name =  '<tr><td>' + data.titles.family_name_en + ', ' +
-        data.titles.given_name_en + '</td></tr>';
-      let name_py = '<tr><td>' + data.titles.family_name_py + ', ' +
-        data.titles.given_name_py + '</td></tr>';
-      let loc = '<tr><td>' + data.loc.location_name + '</td></tr>';
+      let lastName = this.capitalize(data.titles.family_name_en);
+      let firstName = this.capitalize(data.titles.given_name_en);
+      let lastNamePy = this.capitalize(data.titles.family_name_py);
+      let firstNamePy = this.capitalize(data.titles.given_name_py);
+      let sex = this.capitalize(data.gender);
+      let origin = this.capitalize(data.nationality);
+      let location = this.capitalize(data.loc.location_name);
+
+
+      let start = '<div><table><tbody>';
+      let name =  '<tr><td>' + lastName + ', ' + firstName + '</td></tr>';
+      let name_py = '<tr><td>' + lastNamePy + ', ' + firstNamePy + '</td></tr>';
+      let gender = '<tr><td>' + sex + '</td></tr>';
+      let nationality = '<tr><td>' + origin + '</td></tr>';
+      let loc = '<tr><td>' + location + '</td></tr>';
       let time = '<tr><td>' + data.time.birth_year + '-' + data.time.death_year + '</td></tr>';
-      let end = '</tbody> </table> </div>'
-      let popupContent = start + name + name_py + loc + time + end;
+      let end = '</tbody></table> </div>';
+
+      let popupContent = start + name + name_py + gender + nationality + loc + time + end;
       popupContent = popupContent.replace(/, N\/A/g, '').replace(/N\/A-N\/A/g, '').replace(/N\/A/g, '');
       return popupContent;
+    },
+    capitalize(string){
+      let capitalized = "";
+      let words = string.split(" ");
+      words.forEach( (w) =>
+        capitalized += w.charAt(0).toUpperCase() + w.slice(1) + " "
+      )
+      return capitalized;
     }
   }
 }
