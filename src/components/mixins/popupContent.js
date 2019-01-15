b86e43d5de5e6f4437af0618070c283359c9e58c
Ben <bengetch@gmail.com>
2019-01-14 20:37:44 -0500

    refactor display fields

--- src/components/mixins/popupContent.js
+++ src/components/mixins/popupContent.js
@@ -83,7 +83,7 @@ export const PopupContent = {
 
       let start = '<div><table><tbody>';
       let name =
-				"<tr><td>"
+				"<tr><td>Name: "
 				+ this.capitalize(data.name)
 				+ "</td></tr>";
       let type =
@@ -102,7 +102,7 @@ export const PopupContent = {
 				"<tr><td>Nationality: "
 				+ this.capitalize(data.nationality)
 				+ "</td></tr>";
-      let relig = "Religious Family: ";
+      let relig = "<tr><td>Religious Family: </td></tr>";
 			if (!(this.capitalize(data.corp_relations.religious_family).indexOf('N/A') > -1))
 			{
 				relig =
@@ -110,7 +110,7 @@ export const PopupContent = {
 					+ this.capitalize(data.corp_relations.religious_family)
 					+ "</td></tr>";
 			}
-			let assoc = "Association: ";
+			let assoc = "<tr><td>Association:</td></tr> ";
       if (!(this.capitalize(data.corp_relations.association).indexOf('N/A') > -1))
       {
 				assoc =
@@ -135,32 +135,50 @@ export const PopupContent = {
        * @return       an HTML string to use as popup content
        */
 
-      let corpName = this.capitalize(data.name);
-      let location = this.capitalize(data.loc.location_name);
-      let origin = this.capitalize(data.nationality);
-      let religion = this.capitalize(data.corp_relations.religious_family);
-      let association = this.capitalize(data.corp_relations.association);
-      let child_co = 'Child Company: ' + this.capitalize(data.child_inst_name);
+      let start = "<div><table><tbody>";
+      let name = "<tr><td>Corporate Name:</td></tr> ";
+      if (!(this.capitalize(data.name).indexOf('N/A') > -1)) {
+				name =
+					"<tr><td>Corporate Name: "
+					+ this.capitalize(data.name)
+					+ "</td></tr>";
+			}
+      let loc = "<tr><td>Location: "
+				+ this.capitalize(data.loc.location_name)
+				+ ", "
+				+ data.time.start_year
+				+ "</td></tr>";
+      let time =
+				"<tr><td>Start Year: "
+				+ data.time.start_year
+				+ "</td></tr>";
+			let nationality =
+				"<tr><td>Nationality: "
+				+ this.capitalize(data.nationality)
+				+ "</td></tr>";
+      let relig =
+				"<tr><td>Religion :"
+				+ this.capitalize(data.corp_relations.religious_family)
+				+ "</td></tr>";
+      let assoc =
+				"<tr><td>Association: "
+				+ this.capitalize(data.corp_relations.association)
+				+ "</td></tr>";
+      let child =
+				"<tr><td>Institution Name: "
+				+ this.capitalize(data.child_inst_name)
+				+ "</td></tr>";
+      let end = "</tbody></table></div>";
 
-      let start = '<div><table><tbody>';
-      let name = '<tr><td>' + corpName + '</td></tr>';
-      if (corpName.indexOf('N/A') > -1){
-        name = '';
-      }
-      let loc_and_time = '<tr><td>' + location + ', ' + data.time.start_year + '</td></tr>';
-      let nationality = '<tr><td>' + origin + '</td></tr>';
-      let relig = '<tr><td>' + religion + '</td></tr>';
-      let assoc = '<tr><td>' + association + '</td></tr>';
-      let child = '<tr><td>' + child_co + '</td></tr>';
+      let popupContent =
+				start + name + loc
+				+ time + nationality + relig
+				+ assoc + child + end;
 
-      let end = '</tbody></table></div>';
-
-      let popupContent = start + name + loc_and_time + nationality + relig + assoc + child + end;
-      popupContent = popupContent
+      return popupContent
 				.replace(/, N\/A/g, '')
 				.replace(/N\/A-N\/A/g, '')
 				.replace(/N\/A/g, '');
-      return popupContent;
     },
     capitalize(string){
       let capitalized = "";
